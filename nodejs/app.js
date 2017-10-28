19a39f54a4108cc77a6d3b4681d3fcecdd357594
Nazmus Sakib <sukku.mia@gmail.com>
2017-10-28 07:26:25 +0600

    node introduction

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,6 +1,7 @@
+var cat = require('cat-me');
+var jokes = require('knock-knock-jokes');
 
 console.log("hello from node");
 
-var cat = require('cat-me');
-
+console.log(jokes());
 console.log(cat());
